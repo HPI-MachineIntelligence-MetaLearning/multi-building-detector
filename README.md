875df6243c8a8d076551a29c99effaadcdbf88aa
Fabian Paul <fabian.paul@student.hpi.de>
2017-12-07 16:57:56 +0100

    Make run setup configurable

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # multi-building-detector
-Multibox detection framework improved with metric learning
+Multibox detection framework with interchangeable architectures
+
+#Installation
+You need to install miniconda first. 
+
+##GPU Install  
+```
+conda env create -f environment-gpu.yml
+source activate multi-building-detector-gpu
+
+```
+
+##CPU Install
+```
+conda env create -f environment.yml
+source activate multi-building-detector
+
+```
+
+
+#Configuration
+Save a new `*.yml` file in the config directory.
+Currently following fields are supported:
+
+```
+input: <data input path>
+output: <training result output path>
+batch_size: <batch size>
+iterator: <chainer iterator class>
+device: <gpu index or -1 for cpu>
+```
+
+#Usage
+```
+usage: python -m multibuildingdetector [-h] -c CONFIG
+
+Running configured multibox detector
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        Path to config
+
+```
--- multibuildingdetector/__main__.py
+++ multibuildingdetector/__main__.py
@@ -1,53 +1,56 @@
 import chainer
+import argparse
+import yaml
 
 from chainer.optimizer import WeightDecay
 from chainercv.extensions import DetectionVOCEvaluator
 from chainercv.links import SSD300
 from chainercv.links.model.ssd import GradientScaling
-from multibuildingdetector.transformers.augmentation import ImageAugmentation
-from .ssdtrainchain import MultiboxTrainChain
+from multibuildingdetector.tranforms.augmentation import ImageAugmentation
+from multibuildingdetector.multiboxtrainchain import MultiboxTrainChain
 from .readers import xmldataset
 from chainer.datasets import TransformDataset
 from chainer.training import extensions
 
 
-def main():
+def load_config(path):
+    with open(path, 'r') as f:
+        return yaml.load(f)
+
+
+def run(input, output, batch_size, iterator='SerialIterator', device=-1):
     model = SSD300(n_fg_class=len(xmldataset.LABEL_NAMES),
                    pretrained_model='imagenet')
     model.use_preset('evaluate')
     train_chain = MultiboxTrainChain(model)
 
-    # chainer.cuda.get_device_from_id(1).use()
-    # model.to_gpu()
-
-    data_path = '../labeled_images/'
-    OUTPUT = 'result'
-    BATCH_SIZE = 32
+    if device > 0:
+        chainer.cuda.get_device_from_id(device).use()
+        model.to_gpu()
 
     train = TransformDataset(
-        xmldataset.XMLDataset(data_path),
+        xmldataset.XMLDataset(input),
         ImageAugmentation(model.coder, model.insize, model.mean))
-    train_iter = chainer.iterators.MultiprocessIterator(train, BATCH_SIZE)
+    train_iter = getattr(chainer.iterators, iterator)(train, batch_size)
 
-    test = xmldataset.XMLDataset(data_path, split='test')
+    test = xmldataset.XMLDataset(input, split='test')
     test_iter = chainer.iterators.SerialIterator(
-        test, BATCH_SIZE, repeat=False, shuffle=False)
+        test, batch_size, repeat=False, shuffle=False)
 
     optimizer = chainer.optimizers.Adam()
     optimizer.setup(train_chain)
 
-    # Need to find explaination
     for param in train_chain.params():
         if param.name == 'b':
             param.update_rule.add_hook(GradientScaling(2))
         else:
             param.update_rule.add_hook(WeightDecay(0.0005))
 
-    updater = chainer.training.StandardUpdater(train_iter, optimizer)
-    trainer = chainer.training.Trainer(updater, (120000, 'iteration'), OUTPUT)
-    # trainer.extend(
-    #    extensions.ExponentialShift('lr', 0.1, init=1e-3),
-    #    trigger=triggers.ManualScheduleTrigger([80000, 100000], 'iteration'))
+    if device is None:
+        updater = chainer.training.StandardUpdater(train_iter, optimizer)
+    else:
+        updater = chainer.training.StandardUpdater(train_iter, optimizer, device=device)
+    trainer = chainer.training.Trainer(updater, (120000, 'iteration'), output)
 
     trainer.extend(
         DetectionVOCEvaluator(
@@ -64,6 +67,16 @@ def main():
          'validation/main/map']), )
     trainer.extend(extensions.ProgressBar(update_interval=10))
     trainer.run()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Running configured \
+                                                  multibox detector')
+    parser.add_argument('-c', '--config', help='Path to config', required=True)
+
+    config_path = vars(parser.parse_args())['config']
+    config = load_config(config_path)
+    run(**config)
 
 
 if __name__ == '__main__':
