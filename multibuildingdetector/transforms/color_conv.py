6a36adc62a770dce2d593ea80bc3405b9edfe96b
Siegfried Horschig <siegfried.horschig@student.hpi.de>
2017-12-19 16:40:50 +0100

    Change some array access

--- multibuildingdetector/transforms/color_conv.py
+++ multibuildingdetector/transforms/color_conv.py
@@ -67,15 +67,15 @@ def rgb_to_hsv(rgb):
     # -- H channel
     # red is max
     idx = (rgb[:, :, 0] == out_v)
-    out[idx, 0] = (rgb[idx, 1] - rgb[idx, 2]) / delta[idx]
+    out[idx][:, 0] = (rgb[idx][:, 1] - rgb[idx][:, 2]) / delta[idx]
 
     # green is max
-    idx = (rgb[:, :, 1] == out_v)
-    out[idx, 0] = 2. + (rgb[idx, 2] - rgb[idx, 0]) / delta[idx]
+    idx = (rgb[:, :][:, 1] == out_v)
+    out[idx][:, 0] = 2. + (rgb[idx][:, 2] - rgb[idx][:, 0]) / delta[idx]
 
     # blue is max
     idx = (rgb[:, :, 2] == out_v)
-    out[idx, 0] = 4. + (rgb[idx, 0] - rgb[idx, 1]) / delta[idx]
+    out[idx][:, 0] = 4. + (rgb[idx][:, 0] - rgb[idx][:, 1]) / delta[idx]
     out_h = (out[:, :, 0] / 6.) % 1.
     out_h[delta == 0.] = 0.
 
