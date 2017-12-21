0dbdafdf13cda7c2c643cddee221981a2410fd24
Fabian Paul <fabian.paul@student.hpi.de>
2017-12-21 16:40:03 +0100

    Cleanup dataset reset

--- multibuildingdetector/readers/reader.py
+++ multibuildingdetector/readers/reader.py
@@ -5,10 +5,11 @@ from sklearn.model_selection import train_test_split
 from .xmldataset import XMLDataset
 
 
-def load_train_test_set(data_dir, train_size=0.8):
-    img_id_paths = [join(data_dir, ''.join(f.split('.')[:-1])) for f in
-                    listdir(data_dir) if isfile(join(data_dir, f)) and f.split('.')[-1] == 'jpg']
-
-    train_id_paths, test_id_paths = train_test_split(img_id_paths, train_size=train_size)
-    print("Split: {0}, Train size: {1}, Test size: {2}".format(train_size, len(train_id_paths), len(test_id_paths)))
-    return XMLDataset(train_id_paths), XMLDataset(test_id_paths)
+def load_train_test_set(data_dir, train_size=0.8, img_type='jpg'):
+    img_paths = [join(data_dir, ''.join(f.split('.')[:-1])) for f in
+                 listdir(data_dir) if isfile(join(data_dir, f)) and
+                 f.split('.')[-1] == img_type]
+    train, test = train_test_split(img_paths, train_size=train_size)
+    print("Split: {0}, Train size: {1}, Test size: {2}"
+          .format(train_size, len(train), len(test)))
+    return XMLDataset(train), XMLDataset(test)
