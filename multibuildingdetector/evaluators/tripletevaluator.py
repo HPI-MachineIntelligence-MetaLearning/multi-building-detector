d47d1176798e9fe8d5aa1aced9841ae8c04a6b5e
Siegfried Horschig <siegfried.horschig@student.hpi.de>
2018-01-30 21:42:45 +0100

    Add ROC curve for evaluating triplet loss

--- multibuildingdetector/evaluators/tripletevaluator.py
+++ multibuildingdetector/evaluators/tripletevaluator.py
@@ -2,14 +2,19 @@ import copy
 from collections import defaultdict
 from statistics import mean
 import matplotlib.pyplot as plt
+import numpy as np
 
+import chainer
+import chainer.functions as F
 from chainer import reporter
 import chainer.training.extensions
 from multibuildingdetector.loss.ssdtripletloss import SSDTripletLoss
-from scipy.spatial.distance import pdist
+from scipy.spatial.distance import pdist, cdist, euclidean
 from sklearn.decomposition import PCA
+from sklearn import metrics
 
-from chainercv.utils import apply_prediction_to_iterator
+from chainercv.utils import apply_prediction_to_iterator, \
+    non_maximum_suppression
 
 
 class TripletEvaluator(chainer.training.extensions.Evaluator):
@@ -63,7 +68,7 @@ class TripletEvaluator(chainer.training.extensions.Evaluator):
         # delete unused iterator explicitly
         del imgs
 
-        _, mb_confs = pred_values
+        mb_boxs, mb_confs = pred_values
 
         _, gt_labels = gt_values
 
@@ -73,25 +78,93 @@ class TripletEvaluator(chainer.training.extensions.Evaluator):
 
         for labels, confs in zip(gt_labels, mb_confs):
             label_groups.update(SSDTripletLoss._get_label_groups(
-                zip(labels, confs)))
+                self.filter_overlapping_bboxs(mb_boxs, mb_confs,
+                                              gt_labels)))
+        del label_groups[0]
+
+        if self._save:
+            self.plot_roc_curves(label_groups)
+
         for label, feat_v in label_groups.items():
-            if label != 0:
+            avg_dist = -1
+            feat_v = np.array([x.data for x in feat_v])
+            if feat_v.shape[0] > 1:
                 distances = pdist(feat_v)
                 avg_dist = mean(distances)
-                label_name = self.label_names[label - 1]
-                report['avg_dist/{}'.format(label_name)] = avg_dist
+            label_name = self.label_names[label]
+            report['main/avg_dist/{}'.format(label_name)] = avg_dist
+            if self._save:
                 pca = PCA(n_components=2)
                 pca_data = pca.fit_transform(feat_v)
                 plt.scatter([x[0] for x in pca_data],
                             [x[1] for x in pca_data],
                             label=label_name)
-        plt.legend()
         if self._save:
+            plt.legend()
             plt.savefig(self._save_path + '/triplet_scatter.jpg')
+            plt.clf()
         print(report)
-        plt.clf()
 
         observation = dict()
         with reporter.report_scope(observation):
             reporter.report(report, target)
         return observation
+
+    def filter_overlapping_bboxs(self, mb_boxs, mb_confs, gt_labels):
+        confs = []
+        labels = []
+        for box, conf, label in zip(mb_boxs, mb_confs, gt_labels):
+            indices = non_maximum_suppression(box, 0.5)
+            # Add more beatiful version of thi nms-thresh
+
+            confs.append(conf[indices])
+            if chainer.cuda.available:
+                labels.append(label[indices].get())
+            else:
+                labels.append(label[indices])
+        confs = F.concat(confs, axis=0)
+        labels = np.concatenate(labels)
+        return zip(labels, confs)
+
+    def center_point(self, points):
+        center = []
+        for i in range(points.shape[1]):
+            center.append(sum(points[:, i]) / points.shape[1])
+        return np.array(center)
+
+    def plot_roc_curves(self, label_groups):
+        for label_test in label_groups.keys():
+            test_label = self.label_names[label_test]
+            # Note: we don't need to subtract 1 here, we already
+            # deleted the background label
+            for label_center in [k for k in label_groups.keys()
+                                 if k != label_test]:
+                # label_center is the class the centroid is built for,
+                # for label_test the ROC curve will be created
+                center_label = self.label_names[label_center]
+                feat_v = np.array([x.data for x in label_groups[label_center]])
+                ctroid = self.center_point(feat_v)
+                ctroid_arr = np.full(feat_v.shape, ctroid)
+                mean_dist = mean([x[0] for x in cdist(feat_v, ctroid_arr)])
+                predicted = []
+                for feat_center in label_groups[label_center]:
+                    dist = euclidean(ctroid, feat_center.data)
+                    predicted.append(int(dist <= mean_dist))
+                for feat_test in label_groups[label_test]:
+                    dist = euclidean(ctroid, feat_test.data)
+                    predicted.append(int(dist > mean_dist))
+                actual = np.append(np.zeros(len(label_groups[label_center])),
+                                   np.ones(len(label_groups[label_test])))
+                fpr, tpr, _ = metrics.roc_curve(actual, predicted)
+                plt.plot(fpr, tpr, label='ROC curve compared to {}'
+                         .format(center_label))
+                plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+                plt.xlim([0.0, 1.0])
+                plt.ylim([0.0, 1.05])
+                plt.xlabel('False Positive Rate')
+                plt.ylabel('True Positive Rate')
+                plt.title('Receiver operating characteristic {}'
+                          .format(test_label))
+                plt.legend(loc="lower right")
+            plt.savefig(self._save_path + '/{}_ROC.jpg'.format(test_label))
+            plt.clf()
